e64cbdcc428f60f3b0f93620a86936955a2588bc
Kjuly <dev@kjuly.com>
2024-05-22 11:34:07 +0800

    KYPhotoLibrary+AssetURL.swift: Provideo option:useOriginalFilename

--- KYPhotoLibrary/Sources/Asset/KYPhotoLibrary+AssetURL.swift
+++ KYPhotoLibrary/Sources/Asset/KYPhotoLibrary+AssetURL.swift
@@ -13,7 +13,7 @@ extension KYPhotoLibrary {
 
   /// Get the asset URL with the Photo Library scheme "assets-library://".
   ///
-  /// When "`scheme = .library`" (e.g., "assets-library://asset/filename.MP4?id=A45B74E5-003F-49CA-A7DB-FA978A29966C"),
+  /// When "`scheme = .library`" (e.g., "assets-library://asset/asset.MP4?id=A45B74E5-003F-49CA-A7DB-FA978A29966C"),
   ///   `url.checkResourceIsReachable()` will return false because this is not a file system resource URL.
   ///   But we can still use `AVURLAsset(url:)` to load the asset.
   ///
@@ -24,13 +24,16 @@ extension KYPhotoLibrary {
   ///   - assetIdentifier: The asset's unique identifier used in the Photo Library.
   ///   - mediaType: The expected media type of the asset.
   ///   - scheme: URL scheme, default: KYPhotoLibrary.URLScheme.library.
+  ///   - useOriginalFilename: Whether use the original filename in the URL when the "`scheme = .library`",
+  ///     default: false (will use "asset" as the file title); this option will be ignored when "`scheme = .file`".
   ///
   /// - Returns: An asset URL.
   ///
   public static func assetURL(
     with assetIdentifier: String,
     for mediaType: PHAssetMediaType,
-    scheme: KYPhotoLibrary.URLScheme = .library
+    scheme: KYPhotoLibrary.URLScheme = .library,
+    useOriginalFilename: Bool = false
   ) async throws -> URL {
 
     guard let asset: PHAsset = await assetFromIdentifier(assetIdentifier, for: mediaType) else {
@@ -45,19 +48,26 @@ extension KYPhotoLibrary {
       }
     }
 
-    guard let assetResource: PHAssetResource = assetResource(for: asset) else {
-      throw AssetError.assetNotFound(assetIdentifier)
-    }
     // e.g.
     // -  assetLocalIdentifier: 809E4CCC-7AC3-4139-9512-0CD93FFEE2C5/L0/001
     // - uniformTypeIdentifier: public.mpeg-4 (kUTTypeMPEG4)
     // -      originalFilename: Title.MP4
     //
-    //guard let fileExtension: String = UTType.ky_getFileExtensionFromUniformTypeIdentifier(assetResource.uniformTypeIdentifier) else {
-    //  throw AssetError.unsupportedMediaType(mediaType.rawValue)
-    //}
-    let filename: String = (assetResource.originalFilename.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
-                            ?? assetResource.originalFilename)
+    guard let assetResource: PHAssetResource = assetResource(for: asset) else {
+      throw AssetError.assetNotFound(assetIdentifier)
+    }
+
+    var filename: String
+    if useOriginalFilename {
+      filename = (assetResource.originalFilename.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed)
+                  ?? assetResource.originalFilename)
+    } else {
+      guard let fileExtension: String = UTType.ky_getFileExtensionFromUniformTypeIdentifier(assetResource.uniformTypeIdentifier) else {
+        throw AssetError.unsupportedMediaType(mediaType.rawValue)
+      }
+      filename = "asset.\(fileExtension)"
+    }
+
     let identifier: String = assetIdentifier.components(separatedBy: "/").first ?? assetIdentifier
     let filePath: String = "assets-library://asset/\(filename)?id=\(identifier)"
     guard let url = URL(string: filePath) else {
